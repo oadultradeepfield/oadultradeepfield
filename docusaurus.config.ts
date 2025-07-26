f957b8485f0d8c93d0b70fc5bee9d734203ca5f3
Phanuphat Srisukhawasu <phanuphat.srisukhawasu@gmail.com>
2025-07-26 12:26:05 +0800

    feat: Init project and migrate blogs from hugo

---
+++ docusaurus.config.ts
@@ -0,0 +1,139 @@
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+  title: "Phanuphat Srisukhawasu",
+  tagline:
+    "Hi! I’m Oad. I am an Astronomy Olympiad Medalist turned Computer Science major. On this website, I share my projects and cool findings I’ve encountered along my learning and building journeys.",
+  favicon: "img/favicon.ico",
+
+  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+  future: {
+    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+  },
+
+  // Set the production url of your site here
+  url: "https://phanuphats.com",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "oadultradeepfield", // Usually your GitHub org/user name.
+  projectName: "oadultradeepfield", // Usually your repo name.
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      {
+        docs: false,
+        blog: {
+          showReadingTime: true,
+          feedOptions: {
+            type: ["rss", "atom"],
+            xslt: true,
+          },
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          // editUrl:
+          //   "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          // Useful options to enforce blogging best practices
+          onInlineTags: "warn",
+          onInlineAuthors: "warn",
+          onUntruncatedBlogPosts: "warn",
+          remarkPlugins: [remarkMath],
+          rehypePlugins: [rehypeKatex],
+        },
+        theme: {
+          customCss: "./src/css/custom.css",
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  stylesheets: [
+    {
+      href: "https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css",
+      type: "text/css",
+      integrity:
+        "sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM",
+      crossorigin: "anonymous",
+    },
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: "img/docusaurus-social-card.jpg",
+    navbar: {
+      title: "oadultradeepfield",
+      logo: {
+        alt: "oadultradeepfield Logo",
+        src: "img/oadultradeepfield.jpg",
+      },
+      items: [
+        { to: "/project", label: "Project", position: "left" },
+        { to: "/blog", label: "Blog", position: "left" },
+        {
+          href: "https://github.com/oadultradeepfield",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Quick Links",
+          items: [
+            {
+              label: "Project",
+              to: "/project",
+            },
+            {
+              label: "Blog",
+              to: "/blog",
+            },
+          ],
+        },
+        {
+          title: "More",
+          items: [
+            {
+              label: "LinkedIn",
+              href: "https://www.linkedin.com/in/phanuphats",
+            },
+            {
+              label: "GitHub",
+              href: "https://github.com/oadultradeepfield",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Phanuphat (Oad) Srisukhawasu. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
---
+++ src/pages/index.tsx
@@ -0,0 +1,62 @@
+import type { ReactNode } from "react";
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import HomepageFeatures from "@site/src/components/HomepageFeatures";
+import Heading from "@theme/Heading";
+
+import { BookOpen } from "lucide-react";
+import styles from "./index.module.css";
+
+import ResumePdfUrl from "@site/static/resume/phanuphat_srisukhawasu_resume.pdf";
+
+function HomepageHeader() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
+      <div className="container">
+        <Heading as="h1" className="hero__title">
+          {siteConfig.title}
+        </Heading>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div
+          className={styles.buttons}
+          style={{ display: "flex", gap: "1rem", flexWrap: "wrap" }}
+        >
+          <Link
+            className="button button--secondary button--lg"
+            to="/blog"
+            style={{ display: "flex", alignItems: "center", gap: "0.5rem" }}
+          >
+            <BookOpen size={18} />
+            Read My Blog
+          </Link>
+          <Link
+            className="button button--secondary button--lg"
+            to={ResumePdfUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            View Resume <span style={{ marginLeft: 4 }}>&rarr;</span>
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default function Home(): ReactNode {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <Layout
+      title="oadultradeepfield"
+      description="I am an Astronomy Olympiad Medalist turned Computer Science major. Currently, I am focusing on two things I enjoy: building interactive tools that help others learn complex concepts quickly and applying technology to create positive impact in non-technical fields."
+    >
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
+    </Layout>
+  );
+}
